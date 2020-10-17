a2de06fcbeefab04baafc8f1ca059b8702e489b6
Idan Elhalwani <idan@elhalwani.com>
2020-10-17 15:19:33 +0300

    Add toggleable legacy mode to component map

--- crud-framework-utils/src/main/java/com/antelopesystem/crudframework/utils/component/componentmap/configuration/ComponentMapConfiguration.kt
+++ crud-framework-utils/src/main/java/com/antelopesystem/crudframework/utils/component/componentmap/configuration/ComponentMapConfiguration.kt
@@ -1,6 +1,9 @@
 package com.antelopesystem.crudframework.utils.component.componentmap.configuration
 
 import com.antelopesystem.crudframework.utils.component.componentmap.ComponentMapPostProcessor
+import com.antelopesystem.crudframework.utils.component.componentmap.LegacyComponentMapHandler
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
@@ -10,7 +13,15 @@ import org.springframework.core.annotation.Order
 class ComponentMapConfiguration {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ConditionalOnProperty("crudframework.component-map.legacy-mode", havingValue = "false", matchIfMissing = true)
     fun componentMapPostProcessor(): ComponentMapPostProcessor {
         return ComponentMapPostProcessor()
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ConditionalOnProperty("crudframework.component-map.legacy-mode", havingValue = "true")
+    fun legacyComponentMapHandler(): LegacyComponentMapHandler {
+        return LegacyComponentMapHandler()
     }
 }
