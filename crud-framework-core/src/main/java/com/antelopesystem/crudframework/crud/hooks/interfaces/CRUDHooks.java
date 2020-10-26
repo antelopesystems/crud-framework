a83beb90f327510941b3d636594b9478f6802d16
Maisam Mansour <maisamm@xsites.co.il>
2020-10-26 13:35:27 +0200

    Use generics in crudHooks

--- crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/hooks/interfaces/CRUDHooks.java
+++ crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/hooks/interfaces/CRUDHooks.java
@@ -1,22 +1,13 @@
 package com.antelopesystem.crudframework.crud.hooks.interfaces;
 
 import com.antelopesystem.crudframework.model.BaseCrudEntity;
-import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
-
+import org.springframework.core.GenericTypeResolver;
 import java.io.Serializable;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 
 public interface CRUDHooks<ID extends Serializable, Entity extends BaseCrudEntity<ID>> {
 
 	default Class<Entity> getType() {
-		Type[] interfaces = getClass().getGenericInterfaces();
-		for(Type iface : interfaces) {
-			if(iface instanceof ParameterizedType && CRUDHooks.class.isAssignableFrom(((ParameterizedTypeImpl) iface).getRawType())) {
-				return (Class<Entity>) ((ParameterizedType) iface).getActualTypeArguments()[0];
-			}
-		}
-
-		return null; // Not possible
+		Class[] generics = GenericTypeResolver.resolveTypeArguments(getClass(), CRUDHooks.class);
+		return generics[1];
 	}
 }
