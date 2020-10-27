0d344b0891332fc5d87c20c0f2f8a75fe79d00c7
Idan Elhalwani <idan@elhalwani.com>
2020-10-27 14:25:44 +0200

    Change object decorator to use GenericTypeResolver

--- crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/decorator/ObjectDecoratorBase.java
+++ crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/decorator/ObjectDecoratorBase.java
@@ -1,16 +1,18 @@
 package com.antelopesystem.crudframework.crud.decorator;
 
+import com.antelopesystem.crudframework.fieldmapper.transformer.base.FieldTransformerBase;
+import org.springframework.core.GenericTypeResolver;
+
 import java.lang.reflect.ParameterizedType;
 
 public abstract class ObjectDecoratorBase<From, To> implements ObjectDecorator<From, To> {
 
 	@Override
 	public final String getType() {
-		String tClazz = ((Class<From>) ((ParameterizedType) getClass()
-				.getGenericSuperclass()).getActualTypeArguments()[0]).getName();
-		String eClazz = ((Class<To>) ((ParameterizedType) getClass()
-				.getGenericSuperclass()).getActualTypeArguments()[1]).getName();
+		Class<?>[] generics = GenericTypeResolver.resolveTypeArguments(getClass(), ObjectDecoratorBase.class);
+		String fromClazzName = generics[0].getName();
+		String toClazzName = generics[1].getName();
 
-		return tClazz + "_" + eClazz;
+		return fromClazzName + "_" + toClazzName;
 	}
 }
