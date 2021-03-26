090b3411ff0b05b45d15bc86426f2a1f1a16b58b
Idan Elhalwani <idan@elhalwani.com>
2021-03-26 03:08:38 +0300

    Add comments to CRUD hooks

--- crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/hooks/interfaces/IndexHooks.java
+++ crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/hooks/interfaces/IndexHooks.java
@@ -7,14 +7,36 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
 
+/**
+ * This class contains all hook callbacks for the Index operation. It can be implemented
+ * as many times as needed per entity, or even for abstract entities for use with {@link com.antelopesystem.crudframework.crud.annotation.WithHooks}. Implementations of this interface should be declared as Spring beans.
+ * @param <Entity> the entity to listen to
+ * @param <ID> the ID type of the entity
+ */
 public interface IndexHooks<ID extends Serializable, Entity extends BaseCrudEntity<ID>> extends CRUDHooks<ID, Entity> {
 
+	/**
+	 * Called prior to an Index operation.
+	 * Runs after the filter has been validated for the given entity
+	 * @param filter the filter used in the operation
+	 */
 	default void preIndex(@NotNull DynamicModelFilter filter) {
 	}
 
+	/**
+	 * Called during an Index operation.
+	 * If {@link com.antelopesystem.crudframework.crud.model.ReadCRUDRequestBuilder#fromCache} is used, this method will be skipped. It will be called  data access checks and run inside of a read-only transaction.
+	 * @param filter the filter used in the operation
+	 * @param result represents the paginated list of results from the given filter for the give entity
+	 */
 	default void onIndex(@NotNull DynamicModelFilter filter, @NotNull PagingDTO<Entity> result) {
 	}
 
+	/**
+	 * Called after a Index operation
+	 * @param filter the filter used in the operation
+	 * @param result represents the paginated list of results from the given filter for the give entity
+	 */
 	default void postIndex(@NotNull DynamicModelFilter filter, @NotNull PagingDTO<Entity> result) {
 	}
 }
--- crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/hooks/interfaces/UpdateHooks.java
+++ crud-framework-core/src/main/java/com/antelopesystem/crudframework/crud/hooks/interfaces/UpdateHooks.java
@@ -5,14 +5,36 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
 
+/**
+ * This class contains all hook callbacks for the Update operation. It can be implemented
+ * as many times as needed per entity, or even for abstract entities for use with {@link com.antelopesystem.crudframework.crud.annotation.WithHooks}. Implementations of this interface should be declared as Spring beans.
+ * @param <Entity> the entity to listen to
+ * @param <ID> the ID type of the entity
+ */
 public interface UpdateHooks<ID extends Serializable, Entity extends BaseCrudEntity<ID>> extends CRUDHooks<ID, Entity> {
 
+	/**
+	 * Called prior to an Update operation.
+	 * Runs after the entity is verified to be updatable (Not immutable)
+	 * @param entity represents the entity being created
+	 */
 	default void preUpdate(@NotNull Entity entity) {
 	}
 
+	/**
+	 * Called during an Update operation.
+	 * This method will be called after field mapping / object decoration between the
+	 * RO and the entity and data access checks, and before validation.
+	 * This method will run inside of a read/write transaction.
+	 * @param entity represents the entity being updated
+	 */
 	default void onUpdate(@NotNull Entity entity) {
 	}
 
+	/**
+	 * Called after an Update operation
+	 * @param entity represents the entity that has been created
+	 */
 	default void postUpdate(@NotNull Entity entity) {
 	}
 }
