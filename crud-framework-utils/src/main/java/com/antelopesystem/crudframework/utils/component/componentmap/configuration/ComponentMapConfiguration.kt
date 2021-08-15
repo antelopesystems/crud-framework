4cb55e287b661b6c724230f0cc25375545c5a300
Idan Elhalwani <idan@elhalwani.com>
2021-08-15 14:53:43 +0300

    Remove legacy component map handler

--- crud-framework-utils/src/main/java/com/antelopesystem/crudframework/utils/component/componentmap/configuration/ComponentMapConfiguration.kt
+++ crud-framework-utils/src/main/java/com/antelopesystem/crudframework/utils/component/componentmap/configuration/ComponentMapConfiguration.kt
@@ -1,8 +1,6 @@
 package com.antelopesystem.crudframework.utils.component.componentmap.configuration
 
 import com.antelopesystem.crudframework.utils.component.componentmap.ComponentMapPostProcessor
-import com.antelopesystem.crudframework.utils.component.componentmap.LegacyComponentMapHandler
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
@@ -12,15 +10,7 @@ import org.springframework.core.annotation.Order
 class ComponentMapConfiguration {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
-    @ConditionalOnProperty("crudframework.component-map.legacy-mode", havingValue = "false", matchIfMissing = true)
     fun componentMapPostProcessor(): ComponentMapPostProcessor {
         return ComponentMapPostProcessor()
-    }
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    @ConditionalOnProperty("crudframework.component-map.legacy-mode", havingValue = "true")
-    fun legacyComponentMapHandler(): LegacyComponentMapHandler {
-        return LegacyComponentMapHandler()
     }
 }
