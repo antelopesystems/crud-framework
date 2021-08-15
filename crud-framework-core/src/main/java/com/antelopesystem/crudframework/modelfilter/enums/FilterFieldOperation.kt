40c4ad7643e0e9197998e694f90aa073c44c766f
Idan Elhalwani <idan@elhalwani.com>
2021-08-15 14:06:00 +0300

    Convert  FilterFieldOperation to kotlin

--- crud-framework-core/src/main/java/com/antelopesystem/crudframework/modelfilter/enums/FilterFieldOperation.kt
+++ crud-framework-core/src/main/java/com/antelopesystem/crudframework/modelfilter/enums/FilterFieldOperation.kt
@@ -1,31 +1,25 @@
-package com.antelopesystem.crudframework.modelfilter.enums;
+package com.antelopesystem.crudframework.modelfilter.enums
 
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
-
-public enum FilterFieldOperation {
-	Equal(0), NotEqual(1), In(2), NotIn(3), GreaterThan(4), GreaterEqual(5), LowerThan(6), LowerEqual(7), Between(8), Contains(9), IsNull(10), IsNotNull(11), And(12), Or(13), Not(14), ContainsIn(15), NotContainsIn(16), RawJunction(17), StartsWith(18), EndsWith(19), Noop(20);
-
-	private static final Map<Integer, FilterFieldOperation> lookup = new HashMap<Integer, FilterFieldOperation>();
-
-	static {
-		for(FilterFieldOperation s : EnumSet.allOf(FilterFieldOperation.class)) {
-			lookup.put(s.getValue(), s);
-		}
-	}
-
-	private int value;
-
-	FilterFieldOperation(int v) {
-		value = v;
-	}
-
-	public static FilterFieldOperation get(int value) {
-		return lookup.get(value);
-	}
-
-	public int getValue() {
-		return value;
-	}
+enum class FilterFieldOperation {
+    Equal,
+    NotEqual,
+    In,
+    NotIn,
+    GreaterThan,
+    GreaterEqual,
+    LowerThan,
+    LowerEqual,
+    Between,
+    Contains,
+    IsNull,
+    IsNotNull,
+    And,
+    Or,
+    Not,
+    ContainsIn,
+    NotContainsIn,
+    RawJunction,
+    StartsWith,
+    EndsWith,
+    Noop
 }
